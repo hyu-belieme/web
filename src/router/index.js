7c0084e1adc9e39c9df8f97f3156dd9c0405ea8b
ESeokhwan <huan99@hanmail.net>
2023-03-27 17:07:03 +0900

    feat: header component 추가 및 code format

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
+      path: '/stuffs',
+      name: 'stuffs',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/StuffsView.vue')
+    },
+    {
+      path: '/histories',
+      name: 'histories',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/HistoriesView.vue')
+    },
+    {
+      path: '/bulletin',
+      name: 'bulletin',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/BulletinView.vue')
+    },
+    {
+      path: '/users',
+      name: 'users',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/UsersView.vue')
     }
   ]
 })
