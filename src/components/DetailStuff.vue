6795062a13cd54873cf8415612ef7fc84fdafad1
ESeokhwan <huan99@hanmail.net>
2023-03-28 10:36:46 +0900

    feat: detail stuff component 추가

---
+++ src/components/DetailStuff.vue
@@ -0,0 +1,29 @@
+<script setup>
+import StuffInfo from "@/components/StuffInfo.vue";
+import ItemList from "@/components/ItemList.vue";
+</script>
+
+<template>
+  <section class="stuff-detail">
+    <StuffInfo></StuffInfo>
+    <ItemList></ItemList>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.stuff-detail {
+  display: flex;
+  flex-direction: column;
+
+  padding: map-get($map: $spacers, $key: 4);
+  gap: map-get($map: $spacers, $key: 4);
+
+  .stuff-info {
+    flex-grow: 0;
+  }
+
+  .item-list {
+    flex-grow: 1;
+  }
+}
+</style>
