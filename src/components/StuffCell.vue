c9e89b4d616d04d39967703fd09667d9e649b9b6
ESeokhwan <huan99@hanmail.net>
2023-03-28 10:26:22 +0900

    feat: stuff list component 초안 제작

---
+++ src/components/StuffCell.vue
@@ -0,0 +1,70 @@
+<script setup>
+import Tag from "@/components/Tag.vue";
+</script>
+
+<template>
+  <section class="cell">
+    <section class="content">
+      <span class="icon">📻</span>
+      <span class="name">블루투스 스피커</span>
+      <section class="tags">
+        <Tag v-bind="{ color: 'orange', size: 6, content: '4개 남음' }"></Tag>
+      </section>
+    </section>
+    <div class="division-line"></div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.cell {
+  $list-cell-height: 4rem;
+  $padding-size: map-get($spacers, 3);
+  position: relative;
+
+  height: $list-cell-height;
+  padding-left: $padding-size;
+  padding-right: $padding-size;
+
+  &.selected {
+    background-color: $gray-200;
+  }
+
+  .content {
+    width: 100%;
+    height: 100%;
+
+    display: flex;
+    flex-direction: row;
+    gap: map-get($spacers, 1);
+    align-items: stretch;
+
+    .icon {
+      line-height: $list-cell-height;
+      font-size: $h2-font-size;
+    }
+
+    .name {
+      line-height: $list-cell-height;
+      font-size: $h5-font-size;
+      flex-grow: 1;
+    }
+
+    .tags {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      gap: map-get($spacers, 1);
+    }
+  }
+
+  .division-line {
+    $position-bottom: -1;
+
+    width: calc(100% - 2 * $padding-size);
+
+    border-bottom: 1px solid $body-bg;
+    position: absolute;
+    bottom: $position-bottom;
+  }
+}
+</style>
---
+++ src/components/StuffList.vue
@@ -0,0 +1,19 @@
+<script setup>
+import StuffCell from "@/components/StuffCell.vue";
+</script>
+
+<template>
+  <section class="stuff-list">
+    <StuffCell></StuffCell>
+    <StuffCell></StuffCell>
+    <StuffCell></StuffCell>
+    <StuffCell></StuffCell>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.stuff-list {
+  display: flex;
+  flex-direction: column;
+}
+</style>
