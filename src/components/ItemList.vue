e85c273238dc961e7502db1df144b541e4ef6b15
ESeokhwan <huan99@hanmail.net>
2023-03-28 10:32:54 +0900

    feat: item list component 추가

---
+++ src/components/ItemList.vue
@@ -0,0 +1,18 @@
+<script setup>
+import ItemCell from "@/components/ItemCell.vue";
+</script>
+
+<template>
+  <section class="item-list">
+    <ItemCell></ItemCell>
+    <ItemCell></ItemCell>
+    <ItemCell></ItemCell>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.item-list {
+  display: flex;
+  flex-direction: column;
+}
+</style>
