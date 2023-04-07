3b54bfd65e945c272b5139098411c98fd7534c8d
ESeokhwan <huan99@hanmail.net>
2023-04-07 11:42:02 +0900

    feat: 물품 이름과 설명의  수정 모드 구현

--- src/components/StuffInfo.vue
+++ src/components/StuffInfo.vue
@@ -2,9 +2,15 @@
 import type StuffWithItems from "@/models/stuff/StuffWithItems";
 import { useStuffStore } from "@/stores/stuffStore";
 import { storeToRefs } from "pinia";
+import { ref } from "vue";
 
 const stuffStore = useStuffStore();
 const { selectedStuffDetail } = storeToRefs(stuffStore);
+
+const editMode = ref(false);
+const changeMode = () => {
+  editMode.value = !editMode.value;
+};
 </script>
 
 <template>
@@ -12,16 +18,36 @@ const { selectedStuffDetail } = storeToRefs(stuffStore);
     <section class="icon">{{ (selectedStuffDetail as StuffWithItems).thumbnail }}</section>
     <section class="label-and-desc">
       <section class="label">
-        <section class="name">{{ (selectedStuffDetail as StuffWithItems).name }}</section>
+        <section class="name">
+          <input
+            v-if="editMode"
+            type="text"
+            class="form-control w-100 my-2"
+            placeholder="name"
+            aria-label="name"
+            aria-describedby="basic-addon1"
+          />
+          <span v-else>
+            {{ (selectedStuffDetail as StuffWithItems).name }}
+          </span>
+        </section>
         <section class="buttons">
-          <button class="btn btn-primary btn-sm">수정</button>
+          <button class="btn btn-primary btn-sm" @click="changeMode()">수정</button>
           <button class="btn btn-primary btn-sm">추가</button>
         </section>
       </section>
       <div class="desc">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi sint corrupti illum quos.
-        Dolorum architecto illum, veritatis asperiores odio exercitationem impedit natus. Modi
-        magni, aut corporis impedit ullam nemo saepe!
+        <textarea
+          v-if="editMode"
+          class="form-control h-100 fs-7"
+          placeholder="Leave a comment here"
+          id="floatingTextarea"
+        ></textarea>
+        <span v-else class="p-1">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi sint corrupti illum
+          quos. Dolorum architecto illum, veritatis asperiores odio exercitationem impedit natus.
+          Modi magni, aut corporis impedit ullam nemo saepe!
+        </span>
       </div>
     </section>
   </section>
@@ -32,7 +58,7 @@ const { selectedStuffDetail } = storeToRefs(stuffStore);
   display: flex;
   flex-direction: row;
 
-  gap: map-get($map: $spacers, $key: 4);
+  gap: map-get($map: $spacers, $key: 2);
 
   .icon {
     width: 12rem;
@@ -43,12 +69,14 @@ const { selectedStuffDetail } = storeToRefs(stuffStore);
   }
 
   .label-and-desc {
+    flex-basis: 0;
     flex-grow: 1;
 
     display: flex;
     flex-direction: column;
 
     .label {
+      height: 54px;
       display: flex;
       flex-direction: row;
       align-items: center;
@@ -76,9 +104,6 @@ const { selectedStuffDetail } = storeToRefs(stuffStore);
     .desc {
       flex-basis: 0;
       flex-grow: 1;
-
-      border: 0.5px solid $gray-600;
-      padding: map-get($map: $spacers, $key: 1);
 
       font-size: 0.75rem;
       font-weight: 300;
