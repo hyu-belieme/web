564f4615bcbfe6d45a7c9162c58e8c4733e09896
ESeokhwan <huan99@hanmail.net>
2023-04-13 01:12:55 +0900

    fix: bootstrap-icons import 안되던 문제 해결

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
+      "~": fileURLToPath(new URL("./", import.meta.url)),
       "@common": fileURLToPath(new URL("./src/common", import.meta.url)),
       "@core": fileURLToPath(new URL("./src/core", import.meta.url)),
       "@modules": fileURLToPath(new URL("./src/modules", import.meta.url)),
@@ -18,7 +19,10 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: "@import './src/common/assets/styles/main.scss';"
+        additionalData: `
+        @import "./src/common/assets/styles/main.scss"; 
+        @import "./node_modules/bootstrap-icons/font/bootstrap-icons.css";
+        `
       }
     }
   }
