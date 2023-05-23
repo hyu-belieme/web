4fa068298ae504dd5eaba49437a9267504d40b3a
ESeokhwan <huan99@hanmail.net>
2023-05-24 01:46:16 +0900

    [기능] historyList status, time 기준으로 정렬

--- src/modules/histories/queries/HistoryQueries.ts
+++ src/modules/histories/queries/HistoryQueries.ts
@@ -1,6 +1,6 @@
-import type { List } from "immutable";
+import { List, Map } from "immutable";
 import { storeToRefs } from "pinia";
-import { NIL as NIL_UUID } from "uuid";
+import { NIL as NIL_UUID, stringify } from "uuid";
 import { QueryClient, useQuery } from "vue-query";
 
 import {
@@ -11,7 +11,7 @@ import {
 import { historyKeys } from "@common/apis/queryKeys";
 import { useDeptStore } from "@common/stores/deptStore";
 import { useUserStore } from "@common/stores/userStore";
-import type { History } from "@common/types/Models";
+import type { History, HistoryStatus } from "@common/types/Models";
 import { GLOBAL_STALE_TIME } from "@common/utils/Globals";
 import QueryCache from "@common/utils/queryCache";
 
@@ -30,7 +30,8 @@ let historyDetailCache: QueryCache<string, History>;
 
 export const getHistoryListQuery = () => {
   return useQuery<List<History>>(historyKeys.list(), async () => {
-    const historyList = await _getHistoryList();
+    let historyList = await _getHistoryList();
+    historyList = _sortHistoryList(historyList);
     historyStore.updateSelectedId(_getInitialSelectedId(historyList));
     return historyList;
   });
@@ -80,4 +81,54 @@ function _getInitialSelectedId(historyList: List<History>) {
   const selected = historyList.find((value) => value.id === selectedId.value);
   if (selected === undefined) return historyList.get(0)!.id;
   return selected.id;
+}
+
+function _sortHistoryList(historyList: List<History>) {
+  const historyStatusPriority = _makeStatusPriorityMap(_basicHistorySequence);
+  return historyList.sort((left, right) => {
+    let statusComp = _compareHistoryStatus(historyStatusPriority, left.status, right.status);
+    if (statusComp !== 0) return statusComp;
+
+    return _compareHistoryTimes(left, right);
+  });
+}
+
+const _basicHistorySequence = List([
+  List<HistoryStatus>(["REQUESTED"]),
+  List<HistoryStatus>(["USING", "DELAYED"]),
+  List<HistoryStatus>(["LOST"]),
+  List<HistoryStatus>(["RETURNED", "FOUND"]),
+  List<HistoryStatus>(["EXPIRED"])
+]);
+
+function _makeStatusPriorityMap(sequence: List<List<HistoryStatus>>) {
+  let statusPriority: Map<HistoryStatus, number> = Map();
+  sequence.forEach((statuses, index) => {
+    statuses.forEach((e) => (statusPriority = statusPriority.set(e, index)));
+  });
+  return statusPriority;
+}
+
+function _compareHistoryStatus(
+  statusPriority: Map<HistoryStatus, number>,
+  left: HistoryStatus,
+  right: HistoryStatus
+) {
+  if (!statusPriority.has(left) && !statusPriority.has(right)) return 0;
+  if (!statusPriority.has(left)) return -1;
+  if (!statusPriority.has(right)) return 1;
+  return statusPriority.get(left)! - statusPriority.get(right)!;
+}
+
+function _compareHistoryTimes(left: History, right: History) {
+  if (left.status === "REQUESTED" || left.status === "EXPIRED") {
+    return (right.requestedAt || 0) - (left.requestedAt || 0);
+  } else if (left.status === "USING" || left.status === "DELAYED") {
+    return (right.approvedAt || 0) - (left.approvedAt || 0);
+  } else if (left.status === "LOST") {
+    return (right.lostAt || 0) - (left.lostAt || 0);
+  } else if (left.status === "RETURNED" || left.status === "FOUND") {
+    return (right.approvedAt || right.lostAt || 0) - (left.approvedAt || left.lostAt || 0);
+  }
+  return 0;
 }
