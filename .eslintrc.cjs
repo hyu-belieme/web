2dfb58a9c7316e5a2cbc4b6e5bf1a8cbbbac3f35
ESeokhwan <huan99@hanmail.net>
2023-05-31 18:26:14 +0900

    [eslint] eslint airbnb typescript cod style 설정 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,8 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 module.exports = {
   root: true,
   extends: [
+    "airbnb-base",
+    "airbnb-typescript/base",
     "plugin:vue/vue3-essential",
     "eslint:recommended",
     "@vue/eslint-config-typescript",
