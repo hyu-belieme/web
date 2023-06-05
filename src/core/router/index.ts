801780458cc9c2da1d731f8b8a5a29dbd6f15ffb
ESeokhwan <huan99@hanmail.net>
2023-06-05 10:14:02 +0900

    [기능] 로그인 페이지 라우팅 설정

--- src/core/router/index.ts
+++ src/core/router/index.ts
@@ -4,6 +4,15 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/',
+      alias: '/login',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      // component: () => import("../views/StuffsView.vue")
+      component: () => import('@^login/LoginPage.vue'),
+    },
+    {
       path: '/stuffs',
       name: 'stuffs',
       // route level code-splitting
---
+++ src/modules/login/LoginPage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>로그인 페이지</div>
+</template>
+
+<style scoped lang="scss"></style>
