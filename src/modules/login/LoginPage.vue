5393920860a90b29bb9feda5147716e6dca30ddf
ESeokhwan <huan99@hanmail.net>
2023-06-05 11:23:21 +0900

    [기능] 로그인 페이지 레이아웃 제작

--- src/modules/login/LoginPage.vue
+++ src/modules/login/LoginPage.vue
@@ -1,7 +1,42 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>로그인 페이지</div>
+  <section class="login-page">
+    <section class="login-box">
+      <section class="login-box-content">
+        <span>로그인 후에 이용 가능합니다.</span>
+        <button class="btn btn-primary btn-sm">한양 포탈으로 로그인</button>
+      </section>
+    </section>
+  </section>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.login-page {
+  height: 100%;
+
+  display: flex;
+  flex-direction: row;
+  gap: map-get($map: $spacers, $key: 4);
+
+  padding-bottom: map-get($map: $spacers, $key: 4);
+
+  .login-box {
+    width: 100%;
+    height: 100%;
+    background-color: $white;
+
+    .login-box-content {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: map-get($map: $spacers, $key: 2);
+    }
+  }
+}
+</style>
