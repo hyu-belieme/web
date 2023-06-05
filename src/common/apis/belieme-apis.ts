ca6db157577e88afec0946826e4ef271ed1476ec
ESeokhwan <huan99@hanmail.net>
2023-06-05 21:40:25 +0900

    [기능] api token를 이용해 user 로그인 하는 api instance method 제작

--- src/common/apis/belieme-apis.ts
+++ src/common/apis/belieme-apis.ts
@@ -10,6 +10,8 @@ import type StuffRequestBody from '@common/models/StuffRequestBody';
 import StuffWithItems from '@common/models/StuffWithItems';
 import useUserStore from '@common/stores/user-store';
 
+import UserWithSecureInfo from '../models/UserWithSecureInfo';
+
 const userStore = useUserStore();
 const { userToken } = storeToRefs(userStore);
 
@@ -19,13 +21,10 @@ const NETWORK_ERROR: BaseError = {
     '현재 네트워크가 불안하여 서버와 연결이 원할하지 못하거나 서버에 예상하지 못한 문제가 발생하였습니다. 잠시 후 다시 시도해 주세요.',
 } as const;
 
-function createInstance() {
-  return axios.create({
-    baseURL: import.meta.env.VITE_SERVER_API_URL,
-    timeout: 1000,
-    headers: { 'user-token': userToken.value },
-  });
-}
+const API_SERVER_INSTANCE_CONFIG = {
+  baseURL: import.meta.env.VITE_SERVER_API_URL,
+  timeout: 1000,
+};
 
 function handleError(reject: (_?: any) => void) {
   return (error: any) => {
@@ -37,11 +36,30 @@ function handleError(reject: (_?: any) => void) {
   };
 }
 
+export function loginUsingHanyangApiToken(apiToken: string) {
+  const apiUrl = `/universities/37364342-4345-3342-4143-344334433445/login`;
+
+  return new Promise<UserWithSecureInfo>((resolve, reject) => {
+    axios
+      .create({
+        ...API_SERVER_INSTANCE_CONFIG,
+        headers: { 'api-token': apiToken },
+      })
+      .patch<UserWithSecureInfo>(apiUrl)
+      .then((response) => resolve(new UserWithSecureInfo(response.data)))
+      .catch(handleError(reject));
+  });
+}
+
 export function getAllStuffsInDept(deptId: string) {
   const apiUrl = `stuffs?department-id=${deptId}`;
 
   return new Promise<List<Stuff>>((resolve, reject) => {
-    createInstance()
+    axios
+      .create({
+        ...API_SERVER_INSTANCE_CONFIG,
+        headers: { 'user-token': userToken.value },
+      })
       .get<List<Stuff>>(apiUrl)
       .then((response) => {
         let output = List<Stuff>([]);
@@ -58,7 +76,11 @@ export function getStuff(stuffId: string) {
   const apiUrl = `stuffs/${stuffId}`;
 
   return new Promise<StuffWithItems>((resolve, reject) => {
-    createInstance()
+    axios
+      .create({
+        ...API_SERVER_INSTANCE_CONFIG,
+        headers: { 'user-token': userToken.value },
+      })
       .get<StuffWithItems>(apiUrl)
       .then((response) => resolve(new StuffWithItems(response.data)))
       .catch(handleError(reject));
@@ -69,7 +91,11 @@ export function postNewStuff(newStuff: StuffPostRequestBody) {
   const apiUrl = 'stuffs';
 
   return new Promise<StuffWithItems>((resolve, reject) => {
-    createInstance()
+    axios
+      .create({
+        ...API_SERVER_INSTANCE_CONFIG,
+        headers: { 'user-token': userToken.value },
+      })
       .post<StuffWithItems>(apiUrl, newStuff)
       .then((response) => resolve(new StuffWithItems(response.data)))
       .catch(handleError(reject));
@@ -80,7 +106,11 @@ export function editStuff(stuffId: string, newStuffInfo: StuffRequestBody) {
   const apiUrl = `stuffs/${stuffId}`;
 
   return new Promise<StuffWithItems>((resolve, reject) => {
-    createInstance()
+    axios
+      .create({
+        ...API_SERVER_INSTANCE_CONFIG,
+        headers: { 'user-token': userToken.value },
+      })
       .patch<StuffWithItems>(apiUrl, newStuffInfo)
       .then((response) => resolve(new StuffWithItems(response.data)))
       .catch(handleError(reject));
@@ -91,7 +121,11 @@ export function addNewItem(stuffId: string) {
   const apiUrl = 'items';
 
   return new Promise<StuffWithItems>((resolve, reject) => {
-    createInstance()
+    axios
+      .create({
+        ...API_SERVER_INSTANCE_CONFIG,
+        headers: { 'user-token': userToken.value },
+      })
       .post<StuffWithItems>(apiUrl, {
         stuffId,
       })
@@ -104,7 +138,11 @@ export function getAllHistoryInDept(deptId: string) {
   const apiUrl = `histories?department-id=${deptId}`;
 
   return new Promise<List<History>>((resolve, reject) => {
-    createInstance()
+    axios
+      .create({
+        ...API_SERVER_INSTANCE_CONFIG,
+        headers: { 'user-token': userToken.value },
+      })
       .get<List<History>>(apiUrl, {
         timeout: 5000,
       })
@@ -123,7 +161,11 @@ export function getAllRequesterHistoryInDept(deptId: string, userId: string) {
   const apiUrl = `histories?department-id=${deptId}&requester-id=${userId}`;
 
   return new Promise<List<History>>((resolve, reject) => {
-    createInstance()
+    axios
+      .create({
+        ...API_SERVER_INSTANCE_CONFIG,
+        headers: { 'user-token': userToken.value },
+      })
       .get<List<History>>(apiUrl)
       .then((response) => {
         let output = List<History>([]);
@@ -140,7 +182,11 @@ export function getHistory(historyId: string) {
   const apiUrl = `histories/${historyId}`;
 
   return new Promise<History>((resolve, reject) => {
-    createInstance()
+    axios
+      .create({
+        ...API_SERVER_INSTANCE_CONFIG,
+        headers: { 'user-token': userToken.value },
+      })
       .get<History>(apiUrl)
       .then((response) => resolve(new History(response.data)))
       .catch(handleError(reject));
@@ -151,7 +197,11 @@ export function rentStuff(stuffId: string) {
   const apiUrl = `stuffs/${stuffId}/reserve`;
 
   return new Promise<History>((resolve, reject) => {
-    createInstance()
+    axios
+      .create({
+        ...API_SERVER_INSTANCE_CONFIG,
+        headers: { 'user-token': userToken.value },
+      })
       .patch<History>(apiUrl)
       .then((response) => resolve(new History(response.data)))
       .catch(handleError(reject));
@@ -162,7 +212,11 @@ export function rentItem(itemId: string) {
   const apiUrl = `items/${itemId}/reserve`;
 
   return new Promise<History>((resolve, reject) => {
-    createInstance()
+    axios
+      .create({
+        ...API_SERVER_INSTANCE_CONFIG,
+        headers: { 'user-token': userToken.value },
+      })
       .patch<History>(apiUrl)
       .then((response) => resolve(new History(response.data)))
       .catch(handleError(reject));
@@ -173,7 +227,11 @@ export function reportLostItem(itemId: string) {
   const apiUrl = `items/${itemId}/lost`;
 
   return new Promise<History>((resolve, reject) => {
-    createInstance()
+    axios
+      .create({
+        ...API_SERVER_INSTANCE_CONFIG,
+        headers: { 'user-token': userToken.value },
+      })
       .patch<History>(apiUrl)
       .then((response) => resolve(new History(response.data)))
       .catch(handleError(reject));
@@ -184,7 +242,11 @@ export function approveItem(itemId: string) {
   const apiUrl = `items/${itemId}/approve`;
 
   return new Promise<History>((resolve, reject) => {
-    createInstance()
+    axios
+      .create({
+        ...API_SERVER_INSTANCE_CONFIG,
+        headers: { 'user-token': userToken.value },
+      })
       .patch<History>(apiUrl)
       .then((response) => resolve(new History(response.data)))
       .catch(handleError(reject));
@@ -195,7 +257,11 @@ export function returnItem(itemId: string) {
   const apiUrl = `items/${itemId}/return`;
 
   return new Promise<History>((resolve, reject) => {
-    createInstance()
+    axios
+      .create({
+        ...API_SERVER_INSTANCE_CONFIG,
+        headers: { 'user-token': userToken.value },
+      })
       .patch<History>(apiUrl)
       .then((response) => resolve(new History(response.data)))
       .catch(handleError(reject));
@@ -206,7 +272,11 @@ export function cancelItem(itemId: string) {
   const apiUrl = `items/${itemId}/cancel`;
 
   return new Promise<History>((resolve, reject) => {
-    createInstance()
+    axios
+      .create({
+        ...API_SERVER_INSTANCE_CONFIG,
+        headers: { 'user-token': userToken.value },
+      })
       .patch<History>(apiUrl)
       .then((response) => resolve(new History(response.data)))
       .catch(handleError(reject));
