830e58367f9e471770cea8bee10b730930aa20f6
ESeokhwan <huan99@hanmail.net>
2023-06-07 16:27:36 +0900

    [기능] user mode 변경 및 로그아웃 기능 추가

--- src/common/components/TheHeader/TheHeader.vue
+++ src/common/components/TheHeader/TheHeader.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia';
+import { computed } from 'vue';
 
 import useUserModeStore from '@common/stores/user-mode-store';
 
 const userModeStore = useUserModeStore();
 const { userMode } = storeToRefs(userModeStore);
 
+const changeUserModeLabel = computed(() =>
+  userMode.value === 'USER' ? '관리자 모드로' : '사용자 모드로'
+);
+
 function changeUserMode() {
   if (userMode.value === 'USER') userModeStore.updateUserMode('STAFF');
   else userModeStore.updateUserMode('USER');
+}
+
+function logout() {
+  sessionStorage.removeItem('user-info');
+  localStorage.removeItem('user-token');
 }
 </script>
 
@@ -23,7 +33,14 @@ function changeUserMode() {
     <RouterLink to="/users" class="m-4">유저관리</RouterLink>
   </nav>
   <section class="flex-grow-0">
-    <i class="icon bi bi-person" @click="changeUserMode()"></i>
+    <i class="icon bi bi-person" data-bs-toggle="dropdown" aria-expanded="false"></i>
+    <ul class="dropdown-menu">
+      <li class="dropdown-item">내 정보</li>
+      <li class="dropdown-item">학과 이동하기</li>
+      <li class="dropdown-item" @click="changeUserMode()">{{ changeUserModeLabel }}</li>
+      <li><hr class="dropdown-divider" /></li>
+      <li><a class="dropdown-item" @click="logout()" href="/login">로그아웃</a></li>
+    </ul>
   </section>
 </template>
 
