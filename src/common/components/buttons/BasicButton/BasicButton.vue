735d9a984692ec124bb00f073083a21bf2135719
ESeokhwan <huan99@hanmail.net>
2023-09-06 14:02:48 +0900

    [기능] Basic Button 컴포넌트 제작

---
+++ src/common/components/buttons/BasicButton/BasicButton.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    content?: string;
+    color?: string;
+    size?: '' | 'sm' | 'lg';
+  }>(),
+  {
+    content: '',
+    color: '',
+    size: '',
+  }
+);
+
+const isContentTooShort = props.content.length <= 3;
+let btnSizeClassPostfix = '';
+if (isContentTooShort) {
+  btnSizeClassPostfix += '-more-x-padding';
+}
+</script>
+
+<template>
+  <button
+    :class="['btn', 'btn-' + color, (size === '' ? 'btn' : 'btn-' + size) + btnSizeClassPostfix]"
+  >
+    {{ content }}
+  </button>
+</template>
+
+<style scoped lang="scss">
+@import '../styles/main';
+
+.btn-gray {
+  $btn-gray-base-background: $gray-100;
+  $btn-gray-base-border-border: $gray-200;
+  @include button-variant(
+    $background: $gray-100,
+    $border: $btn-gray-base-border-border,
+    $hover-background: $gray-300,
+    $hover-border: $btn-gray-base-border-border,
+    $active-background: shade-color($btn-gray-base-background, $btn-active-bg-tint-amount),
+    $active-border: $btn-gray-base-border-border,
+    $disabled-background: $gray-50,
+    $disabled-border: $btn-gray-base-border-border
+  );
+}
+</style>
