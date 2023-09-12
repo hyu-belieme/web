25676aae051001db961db94ca7d4abb4777ba665
ESeokhwan <huan99@hanmail.net>
2023-09-12 12:56:56 +0900

    [기능] close 버튼 컴포넌트 추가

---
+++ src/common/components/buttons/CloseButton/CloseButton.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button type="button" class="btn-close"></button>
+</template>
+
+<style scoped lang="scss">
+@import '@common/components/buttons/styles/main';
+</style>
