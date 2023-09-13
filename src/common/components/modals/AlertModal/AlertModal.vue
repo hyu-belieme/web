e8950d5d230fff1165a416ead9f487db19a9474e
ESeokhwan <huan99@hanmail.net>
2023-09-13 18:35:13 +0900

    [기능] 새로운 방식으로 작동하는 alertModal 추가

---
+++ src/common/components/modals/AlertModal/AlertModal.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import CloseButton from '@common/components/buttons/CloseButton/CloseButton.vue';
+import BasicModal from '@common/components/modals/BasicModal/BasicModal.vue';
+
+withDefaults(
+  defineProps<{
+    content?: string;
+    hasCloseButton?: boolean;
+    index: number;
+  }>(),
+  {
+    content: '',
+    hasCloseButton: true,
+  }
+);
+</script>
+
+<template>
+  <BasicModal v-bind:index="index" v-bind:no-footer="true">
+    <template v-slot:header>
+      <h5 class="modal-title">오류</h5>
+      <CloseButton v-if="hasCloseButton" @click="$emit('close')" aria-label="Close"></CloseButton>
+    </template>
+    <template v-slot:body>
+      <p class="no-margin-p">{{ content }}</p>
+    </template>
+  </BasicModal>
+</template>
+
+<style scoped lang="scss">
+.no-margin-p {
+  margin: 0;
+}
+</style>
